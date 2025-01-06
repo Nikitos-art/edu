c88e9469da57c5207fe055a3dcf4cb25e16d8b33
Nikita Kurpas <kurpasnikita@gmail.com>
2025-01-06 14:09:59 +0700

    changed ai move

--- tutors_django/apps/games/urls.py
+++ tutors_django/apps/games/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import GamesListView, CrosswordView, ChessView, HanziView, ai_move
+from .views import GamesListView, CrosswordView, ChessView, HanziView
 
 
 app_name = 'games'
